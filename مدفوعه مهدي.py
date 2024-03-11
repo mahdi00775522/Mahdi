9b27e77ce72ea5c59c5cea10f32c1ed1282a86bf
mahdi00775522 <162940865+mahdi00775522@users.noreply.github.com>
2024-03-11 05:55:05 +0300

    Update مدفوعه مهدي.py

--- مدفوعه مهدي.py
+++ مدفوعه مهدي.py
@@ -107,51 +107,7 @@ print()
    
 
 
-import requests,time,pyfiglet,datetime
-now = datetime.datetime.today()
 
-now = datetime.datetime.today()
-mm = str(now.month)
-dd = str(now.day)
-yyyy = str(now.year)
-hour = str(now.hour)
-mi = str(now.minute)
-ss = str(now.second)
-t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
-
-
-hours = (now.hour)
-x = datetime.datetime.now()
-g= datetime.datetime(2024,3,9,0, )
-
-
-if (x.strftime("%x"))>(g.strftime("%x")):
- print('\033[1;32m تم ايقاف الاداه راسل المطور رماد لتفعيل ')
- time.sleep(1)
- print('\033[1;31m المطور رماد @U_Y22    ')
- time.sleep(1)
- print('\033[1;32m  ⚜️ Molven ⚜️')
- time.sleep(1)
- print('\033[1;31m 1')
- time.sleep(1)
- print('\033[1;32m 2')
- time.sleep(1)
- print('\033[1;31m 3')
- time.sleep(1)
- print('\033[1;32m 4')
- time.sleep(1)
- print('\033[1;31m 5')
- time.sleep(1)
- print('\033[1;32m 6')
- time.sleep(1)
- print('\033[1;31m 7')
- time.sleep(1)
- print('\033[1;32m 8')
- time.sleep(1)
- print('\033[1;31m 9')
- time.sleep(1)
- print('\033[1;32m@U_Y22')
- 
  exit()
  open(".token.txt", "w").write(' . . . .')
  print(x)
